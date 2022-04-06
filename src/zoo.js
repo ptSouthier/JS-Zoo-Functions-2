0d13c79d6ec35cfc98660e8b812246b6e4e411b7
Patrick Southier <patrick.southier@hotmail.com>
2022-04-05 22:34:13 -0300

    feat: creating the animalsOlderThan function

--- src/zoo.js
+++ src/zoo.js
@@ -3,10 +3,11 @@ const data = require('./data');
 const { animals, employees, prices, hours } = data;
 
 function animalsByIds(...ids) {
-  return animals.filter((animal) => ids.includes(animal.id))
+  return animals.filter((animal) => ids.includes(animal.id));
 }
 
 function animalsOlderThan(animal, age) {
+  return animals.find((currAnimal) => currAnimal.name === animal).residents.every((resident) => resident.age >= age);
 }
 
 // function employeeByName(employeeName) {
